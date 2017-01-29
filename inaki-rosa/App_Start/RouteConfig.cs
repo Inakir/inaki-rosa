3a3de1f32d0b77d013e1a846fa5df3f82baf5b0a
Inakir <sysadmin@sec.tamu.edu>
2017-01-29 04:49:30 -0600

    change to route

--- inaki-rosa/App_Start/RouteConfig.cs
+++ inaki-rosa/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace inaki_rosa
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "default", id = UrlParameter.Optional }
             );
         }
     }
